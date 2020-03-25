ee284bc0c4a1daba7c6e492dd1471512c69a2790
Atlas123Ma <mabinyi_nobug@163.com>
2020-03-25 21:33:02 +0800

    [Req]add ble client demo [Desc]add ble client demo

---
+++ app/src/main/java/LeXinUUID.java
@@ -0,0 +1,21 @@
+package com.atlas.devicefinder;
+
+import androidx.annotation.Nullable;
+
+public class LeXinUUID {
+    public static final String WEIGHT_UUID = "0000fee8-0000-1000-8000-00805f9b34fb";
+    public static final String WEIGHT_CHARA1 = "003784cf-f7e3-55b4-6c4c-9fd140100a16";
+
+//    public static final String INDICATE_CHARA = "00002a05-0000-1000-8000-00805f9b34fb";
+//    public static final String INDICATE_DES = "00002902-0000-1000-8000-00805f9b34fb";
+
+    public static final String INDICATE_CHARA = "d618d002-6000-1000-8000-000000000000";
+    public static final String INDICATE_DES = "00002902-0000-1000-8000-00805f9b34fb";
+
+    public static final String READ_CHARA = "00002a50-0000-1000-8000-00805f9b34fb";
+
+    public static final String WRITE_CHARA = "0000fec7-0000-1000-8000-00805f9b34fb";
+
+    public static final String WEIXIN_UUID = "0000fee7-0000-1000-8000-00805f9b34fb";
+
+}
--- app/src/main/java/com/atlas/devicefinder/MainActivity.java
+++ app/src/main/java/com/atlas/devicefinder/MainActivity.java
@@ -1,5 +1,6 @@
 package com.atlas.devicefinder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -8,21 +9,33 @@ import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothAdapter.*;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothProfile;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanResult;
 import android.bluetooth.le.ScanSettings;
 import android.content.pm.PackageManager;
+import android.media.audiofx.AudioEffect;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.os.ParcelUuid;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.atlas.devicefinder.LeXinUUID;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import static android.bluetooth.BluetoothAdapter.*;
 import static android.bluetooth.le.ScanSettings.SCAN_MODE_LOW_LATENCY;
@@ -36,6 +49,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button mBtStopBleScan1 = null;
     private Button mBtStartBleScan2 = null;
     private Button mBtStopBleScan2 = null;
+    private BluetoothDevice mTargetDevice = null;
+    private BluetoothGatt mBluetoothGatt = null;
+    private Handler mHandler = null;
+    private static final int MSG_FIND_TARGET = 0;
+    private static final int MSG_READ_CHARA = 1;
+    private static final int MSG_WRITE_CHARA = 2;
+    private static final int MSG_INDICATE = 3;
+    private static final int DELAY_TIMES = 500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,8 +69,146 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
         Log.d(TAG, "mBluetoothLeScanner = " + mBluetoothLeScanner);
 
+        mHandler = new Handler(){
+            @NonNull
+            @Override
+            public void dispatchMessage(Message msg) {
+                Log.d(TAG, "dispatchMessage, msg = " + msg.what);
+                switch (msg.what) {
+                    case MSG_FIND_TARGET:
+                        mTargetDevice = (BluetoothDevice) msg.obj;
+                        stopBleScan2();
+                        mBluetoothGatt = mTargetDevice.connectGatt(getApplicationContext(), false, mBluetoothGattCallback);
+                        break;
+                    case MSG_READ_CHARA:
+                        BluetoothGattCharacteristic gattCharacteristic = (BluetoothGattCharacteristic) msg.obj;
+                        mBluetoothGatt.readCharacteristic(gattCharacteristic);
+                        break;
+                    case MSG_WRITE_CHARA:
+                        BluetoothGattCharacteristic gattCharacteristic2 = (BluetoothGattCharacteristic) msg.obj;
+                        gattCharacteristic2.setValue("0");
+                        mBluetoothGatt.writeCharacteristic(gattCharacteristic2);
+                        break;
+                    case MSG_INDICATE:
+                        BluetoothGattCharacteristic gattCharacteristic3 = (BluetoothGattCharacteristic) msg.obj;
+                        boolean ret =mBluetoothGatt.setCharacteristicNotification(gattCharacteristic3, true);
+                        BluetoothGattDescriptor descriptor = gattCharacteristic3.getDescriptor(UUID.fromString(LeXinUUID.INDICATE_DES));
+                        descriptor.setValue(BluetoothGattDescriptor.ENABLE_INDICATION_VALUE);
+                        mBluetoothGatt.writeDescriptor(descriptor);
+                        break;
+                }
+            }
+        };
     }
 
+
+    private void parseService(List<BluetoothGattService> gattServiceList) {
+        Log.d(TAG, "----- start parseServie -----");
+        for (int i = 0; i < gattServiceList.size(); i++) {
+            BluetoothGattService gattService = (BluetoothGattService)gattServiceList.get(i);
+            UUID uuid = gattService.getUuid();
+            Log.d(TAG, "parseService, service uuid = " + uuid);
+
+            List<BluetoothGattCharacteristic> gattCharacteristics = gattService.getCharacteristics();
+            for (int j = 0; j < gattCharacteristics.size(); j++) {
+                BluetoothGattCharacteristic gattCharacteristic = gattCharacteristics.get(j);
+
+                int properties = gattCharacteristic.getProperties();
+                int permission = gattCharacteristic.getPermissions();
+                Log.d(TAG, "gattCharacteristic, characteristic uuid = " + gattCharacteristic.getUuid() + " properties = " + properties);
+                if (0 !=  (properties & BluetoothGattCharacteristic.PROPERTY_READ)) {
+                    Log.d(TAG, "gattCharacteristic, characteristic uuid = " + gattCharacteristic.getUuid() + " support read");
+                    if (gattCharacteristic.getUuid().toString().equals(LeXinUUID.READ_CHARA)) {
+                        Message msg = new Message();
+                        msg.what = MSG_READ_CHARA;
+                        msg.obj = gattCharacteristic;
+                        mHandler.sendMessageDelayed(msg, DELAY_TIMES);
+                    }
+
+//                        mBluetoothGatt.readCharacteristic(gattCharacteristic);
+                }
+                if ((0 !=  (properties & BluetoothGattCharacteristic.PROPERTY_INDICATE))) {
+                    Log.d(TAG, "gattCharacteristic, characteristic uuid = " + gattCharacteristic.getUuid() + " support indicate");
+                    if (gattCharacteristic.getUuid().toString().equals(LeXinUUID.INDICATE_CHARA)) {
+                        Message msg = new Message();
+                        msg.what = MSG_INDICATE;
+                        msg.obj = gattCharacteristic;
+                        mHandler.sendMessageDelayed(msg, 3*DELAY_TIMES);
+                    }
+                }
+
+                if (0 !=  (properties & BluetoothGattCharacteristic.PROPERTY_NOTIFY)) {
+                    Log.d(TAG, "gattCharacteristic, characteristic uuid = " + gattCharacteristic.getUuid() + " support notify");
+                }
+
+                if ((0 !=  (properties & BluetoothGattCharacteristic.PROPERTY_WRITE))) {
+                    Log.d(TAG, "gattCharacteristic, characteristic uuid = " + gattCharacteristic.getUuid() + " support write");
+                    if (gattCharacteristic.getUuid().toString().equals(LeXinUUID.WRITE_CHARA)) {
+                        Message msg = new Message();
+                        msg.what = MSG_WRITE_CHARA;
+                        msg.obj = gattCharacteristic;
+                        mHandler.sendMessageDelayed(msg, 2*DELAY_TIMES);
+                    }
+                }
+                List<BluetoothGattDescriptor> gattDescriptors = gattCharacteristic.getDescriptors();
+                for (int k = 0; k < gattDescriptors.size(); k++) {
+                    BluetoothGattDescriptor gattDescriptor = (BluetoothGattDescriptor)gattDescriptors.get(k);
+                    Log.d(TAG, "gattDescriptors, descriptor uuid = " + gattDescriptor.getUuid());
+//                        gattDescriptor.setValue(BluetoothGattDescriptor.ENABLE_INDICATION_VALUE);
+//                        mBluetoothGatt.writeDescriptor(gattDescriptor);
+                }
+            }
+        }
+        Log.d(TAG, "----- parseServie end -----");
+    }
+
+    private BluetoothGattCallback mBluetoothGattCallback = new BluetoothGattCallback() {
+
+        public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor,
+                                      int status) {
+            super.onDescriptorWrite(gatt, descriptor, status);
+            Log.d(TAG, "onDescriptorWrite, descriptor = " + descriptor + ", status = " + status);
+        }
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            super.onConnectionStateChange(gatt, status, newState);
+            Log.d(TAG, "onConnectionStateChange, status = " + status + ", newState = " + newState);
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                if (newState == BluetoothProfile.STATE_CONNECTED) {
+                    mBluetoothGatt.discoverServices();
+                } else {
+                    mBluetoothGatt.close();
+                    mBluetoothGatt = mTargetDevice.connectGatt(getApplicationContext(), false, mBluetoothGattCallback);
+                }
+            }
+        }
+
+
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            Log.d(TAG, "onServicesDiscovered, status = " + status);
+            List<BluetoothGattService> gattServicesList = mBluetoothGatt.getServices();
+            parseService(gattServicesList);
+
+        }
+
+        public void onCharacteristicChanged(BluetoothGatt gatt,
+                                            BluetoothGattCharacteristic characteristic) {
+            super.onCharacteristicChanged(gatt, characteristic);
+            Log.d(TAG, "onCharacteristicChanged, characteristic = " + characteristic + ", value = " + characteristic.getValue());
+        }
+
+        public void onCharacteristicWrite(BluetoothGatt gatt,
+                                          BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicWrite(gatt, characteristic, status);
+            Log.d(TAG, "onCharacteristicWrite, characteristic = " + characteristic + ", value = " + characteristic.getStringValue(0) + ", status = " + status);
+        }
+
+        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic,
+                                         int status) {
+            super.onCharacteristicRead(gatt, characteristic, status);
+            Log.d(TAG, "onCharacteristicRead, characteristic = " + characteristic + ", status = " + status + ", value = " + characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT32, 0));
+        }
+    };
     private void initView() {
         mBtStartBleScan1 = (Button)findViewById(R.id.bt_start_blescan1);
         mBtStartBleScan1.setOnClickListener(this);
@@ -81,11 +240,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mBluetoothAdapter.stopLeScan(mLeScanCallback);
     }
 
-    private static ScanCallback mScanCallback = new ScanCallback() {
+
+    private  ScanCallback mScanCallback = new ScanCallback() {
         @Override
         public void onScanResult(int callbackType, ScanResult result) {
             super.onScanResult(callbackType, result);
             Log.d(TAG, "onScanResult, result = " + result);
+            Message msg = new Message();
+            msg.what = MSG_FIND_TARGET;
+            msg.obj = result.getDevice();
+            mHandler.sendMessage(msg);
 
         }
 
@@ -107,7 +271,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //过滤条件
         List<ScanFilter> bleScanFilters = new ArrayList<>();
-        ScanFilter filter = new ScanFilter.Builder().setDeviceAddress("08:7C:BE:48:65:AD").setServiceUuid(ParcelUuid.fromString("0000fee7-0000-1000-8000-00805f9b34fb")).build();
+//        ScanFilter filter = new ScanFilter.Builder().setDeviceAddress("08:7C:BE:48:65:AD").setServiceUuid(ParcelUuid.fromString("0000fee7-0000-1000-8000-00805f9b34fb")).build();
+        ScanFilter filter = new ScanFilter.Builder().setServiceUuid(ParcelUuid.fromString("0000FEE7-0000-1000-8000-00805F9B34FB")).build();
         bleScanFilters.add(filter);
         //扫描设置
         ScanSettings scanSetting = new ScanSettings.Builder().setScanMode(SCAN_MODE_LOW_LATENCY).setCallbackType(ScanSettings.CALLBACK_TYPE_ALL_MATCHES).setMatchMode(ScanSettings.MATCH_MODE_STICKY).build();
